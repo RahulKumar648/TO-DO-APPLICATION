443b9306124f0bdd11f66eb17c72ec5576934cc4
Rahul <rahulkeshri2491@gmail.com>
2021-07-20 23:02:59 +0530

    last commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,20 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/task_list_db');
+// mongoose.connect('mongodb://localhost/task_list_db');
+// const db =mongoose.connection;
 
-const db =mongoose.connection;
 
-db.on('error',console.error.bind(console,'error connecting to db'));
-// up and running then print the message
-db.once('open',function(){
-    console.log('successfully connected to database');
-});
+mongoose.connect(
+    `${process.env.MONGO_URL}`,
+    {useNewUrlParser:true,useUnifiedTopology:true},
+    (err)=>{
+        if(err){console.log(err);}
+    console.log("connected to mongoDB");
+})
+
+
+
+// db.on('error',console.error.bind(console,'error connecting to db'));
+// // up and running then print the message
+// db.once('open',function(){
+//     console.log('successfully connected to database');
+// });
