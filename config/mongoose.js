7d76baecab6683419a340e03b502ccb05fedcbfc
Rahul <rahulkeshri2491@gmail.com>
2021-07-22 22:22:47 +0530

    adding procfile

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,20 +1,20 @@
 const mongoose=require('mongoose');
-// mongoose.connect('mongodb://localhost/task_list_db');
-// const db =mongoose.connection;
+mongoose.connect('mongodb://localhost/task_list_db');
+const db =mongoose.connection;
 
 
-mongoose.connect(
-    `${process.env.MONGO_URL}`,
-    {useNewUrlParser:true,useUnifiedTopology:true},
-    (err)=>{
-        if(err){console.log(err);}
-    console.log("connected to mongoDB");
-})
+// mongoose.connect(
+//     `${process.env.MONGO_URL}`,
+//     {useNewUrlParser:true,useUnifiedTopology:true},
+//     (err)=>{
+//         if(err){console.log(err);}
+//     console.log("connected to mongoDB");
+// })
 
 
 
-// db.on('error',console.error.bind(console,'error connecting to db'));
-// // up and running then print the message
-// db.once('open',function(){
-//     console.log('successfully connected to database');
-// });
+db.on('error',console.error.bind(console,'error connecting to db'));
+// up and running then print the message
+db.once('open',function(){
+    console.log('successfully connected to database');
+});
