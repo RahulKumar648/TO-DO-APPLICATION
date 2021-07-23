b310814315bd242b7d3479e5f8ef9a1f372b5978
Rahul <rahulkeshri2491@gmail.com>
2021-07-23 22:12:37 +0530

    adding port

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.get('/delete-task',function(req,res){
 
 
 // server is listing the request and sending back response
-app.listen(port,function(err){
+app.listen(process.env.port || 3000,function(err){
     if(err){
         console.log('Error',err);
         console.log(`Error in running the server : ${err}`);
@@ -75,3 +75,4 @@ app.listen(port,function(err){
     console.log(`server is running on port:${port}`);
 
 });
+
